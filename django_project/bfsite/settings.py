339d46796ec6215f6d11c2bb8fa366cd5e8081fe
Bastien Gorissen <kadomony@gmail.com>
2024-10-31 11:02:11 +0100

    08.1 2024-10-31 Login and logout templates

--- django_project/bfsite/settings.py
+++ django_project/bfsite/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'bfsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,3 +128,5 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "/"
--- django_project/bfsite/urls.py
+++ django_project/bfsite/urls.py
@@ -21,4 +21,5 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("blog.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ] + debug_toolbar_urls()
